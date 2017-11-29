b0c4e94fb82bcf17186bf3681c258f58222347a7
SavageCore <talk@savagecore.eu>
2017-11-29 01:34:44 +0000

    Support GM4

--- src/gazelle-lastfm-notifications.user.js
+++ src/gazelle-lastfm-notifications.user.js
@@ -9,24 +9,29 @@
 // @include						http*://apollo.rip/user.php?action=notify*
 // @include						http*://notwhat.cd/user.php?action=notify*
 
-// @downloadURL	 https://github.com/SavageCore/gazelle-lastfm-notifications/raw/master/src/gazelle-lastfm-notifications.user.js
+// @downloadURL	 				https://github.com/SavageCore/gazelle-lastfm-notifications/raw/master/src/gazelle-lastfm-notifications.user.js
 // @grant								GM_getValue
 // @grant								GM_setValue
 // @grant								GM_xmlhttpRequest
 // @grant								GM_notification
+// @grant								GM.getValue
+// @grant								GM.setValue
+// @grant								GM.xmlHttpRequest
+// @grant								GM.notification
 //
+// @require      				https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @require					    https://gist.github.com/SavageCore/53cfc8fb0a6c8b4d3ad0be26bf21973c/raw/1434f8f22190c952ff1b47040353d383de552492/shimGMNotification.js
 // @connect					    ws.audioscrobbler.com
 // ==/UserScript==
 
-/*	global document GM_xmlhttpRequest GM_getValue GM_setValue GM_notification shimGMNotification */
+/*	global document GM shimGMNotification */
 /*	eslint new-cap: "off"	*/
 
-document.addEventListener('DOMContentLoaded', (function () {
+document.addEventListener('DOMContentLoaded', (async function () {
 	'use strict';
 	shimGMNotification();
 	createOptionsForm();
-	loadSettings();
+	await loadSettings();
 
 	var fetchButton = document.getElementById('sc_gln_fetch');
 	fetchButton.addEventListener('click', function () {
@@ -46,7 +51,7 @@ document.addEventListener('DOMContentLoaded', (function () {
 	function getTopArtists(options) {
 		if (options.userName && options.apiKey && options.period && options.limit) {
 			var artistString = '';
-			GM_xmlhttpRequest({
+			GM.xmlHttpRequest({
 				method: 'GET',
 				url: 'https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=' + options.userName + '&api_key=' + options.apiKey + '&period=' + options.period + '&limit=' + options.limit + '&format=json',
 				onload: function (response) {
@@ -64,7 +69,7 @@ document.addEventListener('DOMContentLoaded', (function () {
 						artistString = artistString.replace(/(,$)/, '');
 						artistElement.innerText = artistString;
 					} else {
-						GM_notification({
+						GM.notification({
 							text: response.statusText,
 							title: 'Error: Response Status (' + response.status + ')',
 							timeout: 6000
@@ -73,7 +78,7 @@ document.addEventListener('DOMContentLoaded', (function () {
 				}
 			});
 		} else {
-			GM_notification({
+			GM.notification({
 				text: 'Please fill out all fields',
 				title: 'Error:',
 				timeout: 6000
@@ -85,7 +90,7 @@ document.addEventListener('DOMContentLoaded', (function () {
 	function getLovedArtists(options) {
 		if (options.userName && options.apiKey && options.limit) {
 			var artistArray = [];
-			GM_xmlhttpRequest({
+			GM.xmlHttpRequest({
 				method: 'GET',
 				url: 'https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=' + options.userName + '&api_key=' + options.apiKey + '&limit=' + options.limit + '&format=json',
 				onload: function (response) {
@@ -108,7 +113,7 @@ document.addEventListener('DOMContentLoaded', (function () {
 						titleElement.value = 'Top ' + limit + ' Last.FM artists based on loved tracks';
 						artistElement.innerText = artistArray.join(',');
 					} else {
-						GM_notification({
+						GM.notification({
 							text: response.statusText,
 							title: 'Error: Response Status (' + response.status + ')',
 							timeout: 6000
@@ -122,14 +127,14 @@ document.addEventListener('DOMContentLoaded', (function () {
 				title: 'Error:',
 				timeout: 6000
 			};
-			GM_notification(notificationDetails);
+			GM.notification(notificationDetails);
 			return;
 		}
 	}
 
-	function createOptionsForm() {
+	async function createOptionsForm() {
 		var element = document.getElementById('filter_form');
-		var viewToggle = GM_getValue('viewToggle', false);
+		var viewToggle = await GM.getValue('viewToggle', false);
 		element.insertAdjacentHTML('beforebegin', '<div class="head"><strong>Last.FM Settings</strong><span style="float: right;"><a href="#" id="sc_gln_showhide" onclick="$(\'#sc_gln_settings\').gtoggle(); this.innerHTML = (this.innerHTML == \'Show\' ? \'Hide\' : \'Show\'); return false;" class="brackets">Hide</a></span></div><table cellpadding="6" cellspacing="1" border="0" width="100%" class="layout border user_options" id="sc_gln_settings"><tbody><tr id="sc_gln_mode_tr"><td class="label tooltip" title="Mode"><strong>Mode</strong></td><td><select name="sc_gln_mode" id="sc_gln_mode"><option value="artists">Artists</option><option value="loved">Loved Tracks</option></select></td></tr><tr id="sc_gln_username_tr"><td class="label tooltip" title="Last.FM username"><strong>Last.FM username</strong></td><td><input type="text" size="40" name="sc_gln_username" id="sc_gln_username" value></td></tr><tr id="sc_gln_apikey_tr"><td class="label tooltip" title="Last.FM API Key"><strong>Last.FM API Key (Get one <a href=\'http://www.last.fm/api/account/create\' target=\'_blank\'>here</a>)</strong></td><td><input type="text" size="40" name="sc_gln_apikey" id="sc_gln_apikey" value></td></tr><tr id="sc_gln_period_tr"><td class="label tooltip" title="Top artists over timespan"><strong>Period</strong></td><td><select name="sc_gln_period" id="sc_gln_period"><option value="overall">Overall</option><option value="7day">7 Days</option><option value="1month">1 Month</option><option value="3month">3 Months</option><option value="6month">6 Months</option><option value="12month">12 Months</option></select></td></tr><tr id="sc_gln_limit_tr"><td class="label tooltip" title="Limit number of Artists"><strong>Number of Artists</strong></td><td><input type="text" size="40" name="sc_gln_limit" id="sc_gln_limit" value></td></tr><tr id="sc_gln_viewtoggle_tr"><td class="label tooltip" title="Hide this options form on page load"><strong>Hide on load</strong></td><td><input type="checkbox" name="sc_gln_viewtoggle" id="sc_gln_viewtoggle"></td></tr><tr><td colspan="2" class="center"><input type="submit" id="sc_gln_submit" value="Save Settings">&nbsp;<input type="submit" id="sc_gln_fetch" value="Fetch"></td></tr></tbody></table><div class="head colhead_dark"></div>');
 		var submitButton = document.getElementById('sc_gln_submit');
 		submitButton.addEventListener('click', saveSettings, false);
@@ -153,35 +158,31 @@ document.addEventListener('DOMContentLoaded', (function () {
 		};
 	}
 
-	function loadSettings() {
-		var mode = document.getElementById('sc_gln_mode');
-		var userName = document.getElementById('sc_gln_username');
-		var apiKey = document.getElementById('sc_gln_apikey');
-		var period = document.getElementById('sc_gln_period');
-		var limit = document.getElementById('sc_gln_limit');
-		var viewToggle = document.getElementById('sc_gln_viewtoggle');
-		selectItemByValue(mode, GM_getValue('mode', 'artists'));
-		userName.value = GM_getValue('userName', '');
-		apiKey.value = GM_getValue('apiKey', '');
-		selectItemByValue(period, GM_getValue('period', 'overall'));
-		viewToggle.checked = GM_getValue('viewToggle', false);
-		limit.value = GM_getValue('limit', '50');
+	async function loadSettings() {
+		var modeVal = await GM.getValue('mode', 'artists');
+		var peroidVal = await GM.getValue('period', 'overall');
+		selectItemByValue(document.getElementById('sc_gln_mode'), modeVal);
+		document.getElementById('sc_gln_username').value = await GM.getValue('userName', '');
+		document.getElementById('sc_gln_apikey').value = await GM.getValue('apiKey', '');
+		selectItemByValue(document.getElementById('sc_gln_period'), peroidVal);
+		document.getElementById('sc_gln_viewtoggle').checked = await GM.getValue('viewToggle', false);
+		document.getElementById('sc_gln_limit').value = await GM.getValue('limit', '50');
 	}
 
-	function saveSettings() {
+	async function saveSettings() {
 		var mode = document.getElementById('sc_gln_mode').value;
 		var userName = document.getElementById('sc_gln_username').value;
 		var apiKey = document.getElementById('sc_gln_apikey').value;
 		var period = document.getElementById('sc_gln_period').value;
 		var limit = document.getElementById('sc_gln_limit').value;
 		var viewToggle = document.getElementById('sc_gln_viewtoggle').checked;
-		GM_setValue('mode', mode);
-		GM_setValue('userName', userName);
-		GM_setValue('apiKey', apiKey);
-		GM_setValue('period', period);
-		GM_setValue('limit', limit);
-		GM_setValue('viewToggle', viewToggle);
-		GM_notification({
+		await GM.setValue('mode', mode);
+		await GM.setValue('userName', userName);
+		await GM.setValue('apiKey', apiKey);
+		await GM.setValue('period', period);
+		await GM.setValue('limit', limit);
+		await GM.setValue('viewToggle', viewToggle);
+		GM.notification({
 			text: 'Settings saved!',
 			title: 'Success:',
 			timeout: 6000
@@ -192,7 +193,9 @@ document.addEventListener('DOMContentLoaded', (function () {
 		for (var i = 0; i < elem.options.length; i++) {
 			if (elem.options[i].value === value) {
 				elem.selectedIndex = i;
+				return true;
 			}
 		}
+		return false;
 	}
 })(), false);
