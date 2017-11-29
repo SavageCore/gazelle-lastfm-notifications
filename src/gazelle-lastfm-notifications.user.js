de36883ceff718fe47a155a2f619061d9b4668fe
SavageCore <talk@savagecore.eu>
2017-11-29 01:41:36 +0000

    0.3.8

--- src/gazelle-lastfm-notifications.user.js
+++ src/gazelle-lastfm-notifications.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name					Gazelle notificatons from last.fm top artists
 // @namespace			http://savagecore.eu/
-// @version				0.2.8
+// @version				0.3.8
 // @description		Generate notifications on Gazelle sites based upon Last.FM top artists.
 // @author				SavageCore
 // @include				http*://redacted.ch/user.php?action=notify*
